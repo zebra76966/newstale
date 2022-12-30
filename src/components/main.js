cee9b46e4514be3463b0d87f06599bf91de922c1
zebra <rs288442@gmail.com>
2022-12-30 23:22:19 +0530

    -Added searchBar

--- src/components/main.js
+++ src/components/main.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import {useEffect, useState} from 'react';
+import Axios from 'axios';
 import Card from './newscard'
 import Header from './header'
 import Footer from './footer'
+
 const Main=()=>{
+
+  const [datas,setData] = useState([]);
+  const [isLoad,setIsload] = useState(false);
+  const [search, setSearch] = useState('');
+
+  useEffect(()=>{
+    setIsload(true);
+    Axios.get("https://cors-anywhere.herokuapp.com/https://ace.qtstage.io/api/v1/collections/entertainment")
+    .then(response=>response)
+    .then(data=>{
+      setData(data.data.items)
+      setIsload(false);
+    })
+    console.log(datas)
+  },[])
+
+
 
   return(
     <>
@@ -12,8 +32,28 @@ const Main=()=>{
       <div className="container">
         <div className="row">
         <h3 className="display-4 fw-bold py-5 text-light text-center">Trending<span style={{color:"#FFB231"}}> Stories</span></h3>
+        {isLoad&&<span className="col-12 my-5 py-5">
+          <div className="loader mx-auto"></div>
+        </span>}
 
-        <Card/>
+{/*Search-bar code Starts ======>*/}
+        <div className="col-md-12 pb-5">
+          <div className="search">
+
+            <input type="text" className="form-control" placeholder="Search Stories"
+              onChange={(e)=>setSearch(e.target.value.toLowerCase())}/>
+            <i class="fa fa-search fs-2"></i>
+          </div>
+        </div>
+
+        {datas.map(pini=>{
+          if((pini.item.headline[0].toLowerCase()).includes(search)){
+
+            return <Card res={pini}/>
+          }
+        })
+        }
+{/*Search-bar code End ======>*/}
 
         </div>
       </div>
--- src/components/newscard.js
+++ src/components/newscard.js
@@ -2,10 +2,8 @@ import React from 'react';
 import {useEffect, useState} from 'react';
 import Axios from 'axios';
 
-const Card = () =>{
-
-  const [isLoad,setIsload] = useState(false);
-  const [datas,setData] = useState([]);
+const Card = (props) =>{
+  console.log(props.res)
   const [images,setImg] = useState([]);
   const [localDat, setLocaldat] = useState(localStorage.getItem('likes')!=null?JSON.parse(localStorage.getItem('likes')):[]);
 
@@ -15,60 +13,42 @@ const Card = () =>{
     localStorage.setItem('likes',JSON.stringify(localDat));
   },[localDat])
 
-  useEffect(()=>{
-    setIsload(true);
-    Axios.get("https://cors-anywhere.herokuapp.com/https://ace.qtstage.io/api/v1/collections/entertainment")
-    .then(response=>response)
-    .then(data=>{
-      setData(data.data.items)
-      setIsload(false);
-    })
-  },[])
+
 
 
   return (
     <>
-    {isLoad&&<span className="col-12 my-5 py-5">
-      <div className="loader mx-auto"></div>
-    </span>}
-    {datas.map((res)=>{
-
-      return(
         <div className="col-12 col-md-6 col-lg-4 p-3">
           <div className="card border-0 h-100 w-100 rounded" style={{background:"rgba(255,255,255,0.5)",boxShadow:"0 0 10px rgba(0,0,0,0.5)", backdropFilter:"blur(15px)"}}>
             <img src="gy.png" className="card-img-top rounded" style={{maxHeight:"200px",objectFit:"cover"}} alt="..."/>
             <div className="card-header d-flex align-items-center justify-content-between">
 
-              <p className="p-0 m-0">Author: <strong>{res.story["author-name"]}</strong></p>
+              <p className="p-0 m-0">Author: <strong>{props.res.story["author-name"]}</strong></p>
 
-              <img src={res.story.authors[0]["avatar-url"]}
+              <img src={props.res.story.authors[0]["avatar-url"]}
               className="rounded-pill"
               style={{width:"40px",height:"40px",objectFit:"cover"}}/>
 
             </div>
             <div className="card-body mb-0">
-              <h5 className="card-title">{res.item.headline[0]}</h5>
+              <h5 className="card-title">{props.res.item.headline[0]}</h5>
               <p className="card-text">Some dummy text to build on the card title.</p>
               <a href="#" className="btn  btn-outline-warning fw-bold">Read More...</a>
             </div>
 
-
-            {!localDat.includes(res.id)?
+{/*fetching data from localStorage and rendering conditionally Start======>*/}
+            {!localDat.includes(props.res.id)?
               <i className="p-4 fs-3 text-danger align-self-end fa-regular fa-heart"
-                id={res.id}
-                onClick={(e)=>{setLocaldat(ini=>{return[...ini,res.id]});}}>
+                id={props.res.id}
+                onClick={(e)=>{setLocaldat(ini=>{return[...ini,props.res.id]});}}>
               </i>:
               <i className="p-4 fs-3 text-danger align-self-end fa-solid fa-heart"
-                id={res.id}
-                onClick={(e)=>{setLocaldat(ini=>ini.filter(function(val){return val!=res.id}))}}>
-              </i>
-           }
+                id={props.res.id}
+                onClick={(e)=>{setLocaldat(ini=>ini.filter(function(val){return val!=props.res.id}))}}>
+              </i>}
+{/*fetching data from localStorage End ======>*/}
           </div>
         </div>
-      )
-
-
-    })}
 
     </>
   )
