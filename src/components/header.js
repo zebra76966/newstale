10453987a35e57c2a094b45ce558549adf0f83b5
zebra <rs288442@gmail.com>
2023-01-01 14:08:47 +0530

    -updated landing page -replaced form with a small text-box and button

--- src/components/header.js
+++ src/components/header.js
@@ -38,8 +38,8 @@ const Header=()=>{
 {/* NAVABAR END ===>*/}
 
 {/* HERO CONTENT START ===>*/}
-        <div className="row d-flex align-items-center justify-content-center p-5" style={{height:'80vh'}}>
-          <div className="col-lg-8 col-md-12 herotxt text-center text-lg-start">
+        <div className="row d-flex align-items-center justify-content-center p-lg-5 p-2" style={{height:'80vh'}}>
+          <div className="col-lg-7 col-md-12 herotxt text-center text-lg-start">
             <h1 className="display-1 text-light"><strong>Facts</strong></h1>
             <h1 className="display-1 text-light"><strong>Real</strong></h1>
             <h1 className="display-1 text-light"><strong>True.</strong></h1>
@@ -47,28 +47,19 @@ const Header=()=>{
               Not Your Average News platform
               NewsTale delivers you the truth. For people by people.
             </p>
-            <a href="#trend" className="btn align-items-center mx-auto mx-lg-0 p-3 fw-bold" style={{background:'#FFB231',color:'white',letterSpacing:'1px'}}>
-              Start Now
-              <i className="fa-solid fa-arrow-right ps-2"></i>
-            </a>
+            <div className="d-lg-block d-none">
+              <a href="#trend" className="btn align-items-center mx-auto mx-lg-0 p-3 fw-bold" style={{background:'#FFB231',color:'white',letterSpacing:'1px'}}>
+                Start Now
+                <i className="fa-solid fa-arrow-right ps-2"></i>
+              </a>
+            </div>  
           </div>
-          <div className="col-lg-4 d-lg-block d-none">
-            <form className="bg-light p-5 rounded" autoComplete="on">
-            <h5 className="fs-5 fw-bold pb-3 text-center">SignUp for our <span style={{color:"#FFB231"}}>Newsletter!</span></h5>
+          <div className="col-lg-5 col-12">
+          <h4 className="fs-4 text-lg-start text-light text-lg-dark pb-3  text-center fw-bold">SignUp for you <span className="bg-dark p-2 text-warning">NewsLetter!</span></h4>
+            <form className="bg-none rounded d-flex" autoComplete="on">
             <hr/>
-              <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-              </div>
-              <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" autoComplete="off"/>
-              </div>
-              <div className="mb-3 form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-              </div>
-              <button type="button" className="btn w-100 p-2 fw-bold" style={{background:'#FC5055',color:'white',letterSpacing:'3px'}}>Submit</button>
+              <input className="form-control border border-3 rounded-0 rounded-start p-3" type="email" placeholder="Your Email Address" aria-label="email"/>
+              <button type="button" className="btn px-lg-4 rounded-end border rounded-0 rounded-end fw-bold" style={{background:'#FC5055',color:'white',letterSpacing:'3px'}}>Submit</button>
             </form>
           </div>
         </div>
